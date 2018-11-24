f3685d73387df59b83fc285eb2677959d6db196d
Theo <theo.denanassy@epitech.eu>
2018-11-24 16:50:15 +0100

    Arrow

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -16,49 +16,34 @@ public class Manager : MonoBehaviour
         public string desc;
     }
 
-
     public Image FoodImage;
     public Text desc, day;
     public Image DollarBar, PlusBar, FoodBar;
     public Image DollarDotSmall, DollarDotLarge, PlusDotSmall, PlusDotLarge, FoodDotSmall, FoodDotLarge;
-
+    public Image RightArrow, LeftArrow;
 
     public Food[] foods;
 
-    // put the following variables in private after some tests
-
-    private float nbDollarMax = 100, nbDollar = 100, nbFood = 100, nbFoodMax = 100, nbPlusMax = 100, nbPlus = 100;
+    private float nbDollarMax = 100, nbDollar = 50, nbFood = 50, nbFoodMax = 100, nbPlusMax = 100, nbPlus = 80;
 
     private int t, i = 1;
 
     private bool alive;
 
+    private float lerpSpeed = 2;
+
     // Use this for initialization
     void Start()
     {
         alive = true;
-        t = Random.Range(0, 45);
+        t = Random.Range(0, 22);
         FoodImage.sprite = foods[t].sprite;
         desc.text = foods[t].desc;
         Debug.Log(foods[t].desc);
-        checkDot(foods[t].Argent, foods[t].Calories, foods[t].Health);
+        CheckDot(foods[t].Argent, foods[t].Calories, foods[t].Health);
     }
 
-    //private bool IsAlive()
-    //{
-    //    if (nbFood <= 0 || nbFood >= 100 ||
-    //        nbDollar <= 0 || nbDollar >= 100 ||
-    //        nbPlus <= 0 || nbPlus >= 100)
-    //    {
-    //        desc.text = "You are dead...";
-    //        return false;
-    //    }
-    //    else
-    //        return true;
-    //    return false;
-    //}
-
-    private void checkDot(int nbDollar, int nbFood, int nbPlus)
+    private void CheckDot(int nbDollar, int nbFood, int nbPlus)
     {
         if (nbDollar >= 10 || nbDollar <= -10)
         {
@@ -107,10 +92,9 @@ public class Manager : MonoBehaviour
         }
     }
 
-    private void SwipeLeft()
+    public void SwipeLeft()
     { 
-        nbFood -= 35;
-        nbDollar += 10;
+        nbFood -= 15;
         nbPlus -= 20;
 
         if (nbFood >= 100)
@@ -123,11 +107,17 @@ public class Manager : MonoBehaviour
         if (nbFood <= 0 || nbPlus <= 0 || nbDollar <= 0)
             alive = false;
 
+        if (foods[t].desc == "Indigestion")
+        {
+            nbFood -= foods[t].Calories;
+            nbPlus -= foods[t].Health;
+        }
+
 
         NewImage(nbFood, nbDollar, nbPlus);
     }
 
-    private void SwipeRight()
+    public void SwipeRight()
     {
         nbFood += foods[t].Calories;
         nbDollar -= foods[t].Argent;
@@ -148,14 +138,10 @@ public class Manager : MonoBehaviour
 
     private void NewImage(float nbFood, float nbDollar, float nbPlus)
     {
-        DollarBar.fillAmount = nbDollar / 100;
-        FoodBar.fillAmount = nbFood / 100;
-        PlusBar.fillAmount = nbPlus / 100;
-
-        t = Random.Range(0, 45);
+        t = Random.Range(0, 22);
         FoodImage.sprite = foods[t].sprite;
         desc.text = foods[t].desc;
-        checkDot(foods[t].Argent, foods[t].Calories, foods[t].Health);
+        CheckDot(foods[t].Argent, foods[t].Calories, foods[t].Health);
         Debug.Log(foods[t].desc);
 
         i++;
@@ -170,6 +156,9 @@ public class Manager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        DollarBar.fillAmount = Mathf.Lerp(DollarBar.fillAmount, nbDollar / 100, Time.deltaTime * lerpSpeed);
+        FoodBar.fillAmount = Mathf.Lerp(FoodBar.fillAmount, nbFood / 100, Time.deltaTime * lerpSpeed);
+        PlusBar.fillAmount = Mathf.Lerp(PlusBar.fillAmount, nbPlus / 100, Time.deltaTime * lerpSpeed);
         if (alive)
         {
             if (Input.GetKeyDown(KeyCode.LeftArrow))
---
+++ Assets/Scripts/SwipeTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeTest : MonoBehaviour {
+
+    public Swipe swipeControls;
+    public RectTransform card;
+    private Vector3 desiredPosition = new Vector3(0, 46, 0);
+
+    private void Update()
+    {
+        if (swipeControls.SwipeLeft)
+            desiredPosition += Vector3.left;
+        if (swipeControls.SwipeRight)
+            desiredPosition += Vector3.right;
+        if (swipeControls.SwipeUp)
+            desiredPosition += Vector3.forward;
+        if (swipeControls.SwipeDown)
+            desiredPosition += Vector3.back;
+
+        Debug.Log(desiredPosition);
+        Debug.Log(card.localPosition);
+        card.transform.position = Vector3.MoveTowards(card.position, desiredPosition, 20f * Time.deltaTime);
+
+        if (swipeControls.Tap)
+            Debug.Log("Tap!");
+    }
+}
